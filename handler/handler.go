a6f2f64ea2949a6707f4269a8a250b7a7c5f84fb
Alvin Rizki <4lvin.rizki@gmail.com>
2019-12-09 00:41:19 +0700

    add toggle provider

--- handler/handler.go
+++ handler/handler.go
@@ -5,6 +5,10 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/cikupin/feature-flag-example/constant"
+
+	"github.com/cikupin/feature-flag-example/common"
+
 	"github.com/checkr/goflagr"
 )
 
@@ -39,10 +43,40 @@ func (h *Handler) ToggleFeature(w http.ResponseWriter, r *http.Request) {
 			fmt.Fprintf(w, "- [%s] %s\n", flag.Key, flag.Description)
 		}
 	}
-	return
 }
 
 // ToggleProvider defines handler for provider toggle simulation
 func (h *Handler) ToggleProvider(w http.ResponseWriter, r *http.Request) {
+	ctx := context.Background()
 
+	flag, err := common.GetFlagByKey(ctx, h.Client, constant.FlagWhatsappNotification)
+	if err != nil {
+		fmt.Fprintf(w, "Error: %s\n", err.Error())
+		return
+	}
+
+	if flag == nil || !flag.Enabled {
+		fmt.Fprintln(w, "Whatsapp feature is not enabled")
+		return
+	}
+
+	segment, err := common.GetSegmentByKey(ctx, h.Client, flag.Id, constant.SegmentDefault)
+	if err != nil {
+		fmt.Fprintf(w, "Error: %s\n", err.Error())
+		return
+	}
+
+	if segment == nil {
+		fmt.Fprintln(w, "No segment found")
+		return
+	}
+
+	for _, dist := range segment.Distributions {
+		if dist.Percent == 100 {
+			fmt.Fprintf(w, "WA notification service : %s\n", dist.VariantKey)
+			return
+		}
+	}
+
+	fmt.Fprintln(w, "No single WA notification service found")
 }
